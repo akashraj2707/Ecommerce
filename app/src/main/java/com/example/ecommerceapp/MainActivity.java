668681ecbfebc3c79c449c61f3a253710b8f8a96
akash <akash.raj.3110@gmail.com>
2020-11-09 03:14:49 +0530

    Ecommerce App

--- app/src/main/java/com/example/ecommerceapp/MainActivity.java
+++ app/src/main/java/com/example/ecommerceapp/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.ecommerceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+     private Button SignUpButton,LoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SignUpButton=findViewById(R.id.signUpButton);
+        LoginButton=findViewById(R.id.loginButton);
+
+
     }
 }
